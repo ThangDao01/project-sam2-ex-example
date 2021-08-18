97202c5264102469940fea0fb7382b9048af297f
Your Name <email_cua_ban>
2021-08-18 16:10:35 +0700

    create-template-user

--- resources/views/admin/data/create.blade.php
+++ resources/views/admin/data/create.blade.php
@@ -32,53 +32,83 @@
                 <form action="/admin/data-support/create" method="post">
                     @csrf
                     <div class="form-group">
-                        <label for="values">Values:</label>
+                        <label class="col-md-3 control-label" for="Values">Values:</label>
+                        <div class="col-md-6">
                         <input type="text" class="form-control" id="values" placeholder="Enter values" name="values">
+                            @error('Values')
+                            <div class="text-danger">* {{ $message }}</div>
+                            @enderror
+                    </div>
                     </div>
                     <div class="form-group">
-                        <label for="words_vi">Words_vi:</label>
+                        <label class="col-md-3 control-label" for="words_vi">Words_vi:</label>
+                        <div class="col-md-6">
                         <input type="text" class="form-control" id="words_vi" placeholder="Enter words_vi"
                                name="words_vi">
                         <div id="words_vi-demo">
                         </div>
+                        </div>
                     </div>
                     <div class="form-group">
-                        <label for="words_en">Words_en:</label>
+                        <label class="col-md-3 control-label" for="words_en">Words_en:</label>
+                        <div class="col-md-6">
                         <input type="text" class="form-control" id="words_en" placeholder="Enter voice_vi"
                                name="words_en">
                         <div id="words_en-demo">
                         </div>
-                    </div>
+                    </div></div>
                     <div class="form-group">
-                        <label for="voice_vi">Voice_vi:</label>
+                        <label class="col-md-3 control-label" for="voice_vi">Voice_vi:</label>
+                        <div class="col-md-6">
                         <input type="text" class="form-control" id="voice_vi" placeholder="Enter voice_vi"
                                name="voice_vi">
                         <div id="voice_vi-demo">
                         </div>
+                        </div>
                     </div>
                     <div class="form-group">
-                        <label for="voice_en">Voice_en:</label>
+                        <label class="col-md-3 control-label" for="voice_en">Voice_en:</label>
+                        <div class="col-md-6">
                         <input type="text" class="form-control" id="voice_en" placeholder="Enter email" name="voice_en">
                         <div id="voice_en-demo">
                         </div>
                     </div>
+                    </div>
                     <div class="form-group">
-                        <label for="images">Images:</label>
+                        <label class="col-md-3 control-label" for="images">Images:</label>
+                        <div class="col-md-6">
                         <input type="file" class="form-control" id="images" placeholder="Enter images" name="images">
                         <div id="images-demo">
                         </div>
+                        </div>
                     </div>
                     <div class="form-group">
-                        <label for="key">Key:</label>
+                        <label class="col-md-3 control-label" for="key">Key:</label>
+                        <div class="col-md-6">
                         <input type="text" class="form-control" id="key" placeholder="Enter Key" name="key">
                     </div>
+                    </div>
                     <div>
+                        <label class="col-md-3 control-label"></label>
+                        <div class="col-md-6 col-xs-11">
                         Key : thể loại , màu
+                        </div>
                     </div>
                     <br>
-                    <button type="submit" id="btn" class="btn btn-primary">Submit</button>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label"></label>
+                        <div class="col-md-6 col-xs-11">
+                            <button class="btn btn-sm btn-primary colorpicker-element">
+                                Submit
+                            </button>
+                            <button class="btn btn-sm btn-default colorpicker-element">
+                                Reset
+                            </button>
+                        </div>
+                    </div>
                 </form>
             </div>
+
         </section>
         <!-- end: page -->
     </section>
