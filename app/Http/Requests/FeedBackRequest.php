d83f172ab16115afb3d9eb428bcb11df6a7c5930
Your Name <email_cua_ban>
2021-08-24 21:33:08 +0700

    template-admin-24/8-quan

--- app/Http/Requests/FeedBackRequest.php
+++ app/Http/Requests/FeedBackRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class DataSupportRequest extends FormRequest
+class FeedBackRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class DataSupportRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return false;
     }
 
     /**
@@ -24,13 +24,11 @@ class DataSupportRequest extends FormRequest
     public function rules()
     {
         return [
-            'values' => 'required',
-            'color' => 'required',
-            'words_en' => 'required',
-            'video' => 'required',
-            'voice_en' => 'required',
-            'images' => 'required',
-            'key' => 'required'
+            //
+            'Account' => 'required',
+            'message' => 'required',
+            'vote' => 'required',
+            'Seen' => 'required',
         ];
     }
 }
--- app/Http/Requests/courseRequest.php
+++ app/Http/Requests/courseRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class DataSupportRequest extends FormRequest
+class courseRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,13 +24,12 @@ class DataSupportRequest extends FormRequest
     public function rules()
     {
         return [
-            'values' => 'required',
-            'color' => 'required',
-            'words_en' => 'required',
-            'video' => 'required',
-            'voice_en' => 'required',
-            'images' => 'required',
-            'key' => 'required'
+            //
+            'CourseName' => 'required',
+            'Price' => 'required',
+            'Description' => 'required',
+            'time_finish' => 'required',
+            'Status' => 'required',
         ];
     }
 }
