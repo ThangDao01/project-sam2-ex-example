7a7ee0a05faf7707fb4a26672c13bcc25d745f05
ThangDao01 <72856471+ThangDao01@users.noreply.github.com>
2021-08-25 02:13:59 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function (){
-    return view('welcome');
-});
+
 Route::get('/admin/index', function () {
     return view('admin.index');
 });
