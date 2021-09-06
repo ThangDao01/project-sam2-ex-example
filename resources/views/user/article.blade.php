3aae500b3c09208435ce96636051e813045456cc
trancuongitpro <cuongtmth2008049@fpt.edu.vn>
2021-09-07 02:16:50 +0700

    huongdan-article

--- resources/views/user/article.blade.php
+++ resources/views/user/article.blade.php
@@ -1,17 +1,14 @@
-{{--@extends('layout.user-layout')--}}
-{{--<title>E&K - {{$description->title}}</title>--}}
-{{--@section('detail')--}}
-{{--    <div class="col-8">--}}
-{{--        {!! $description !!}--}}
-{{--    </div>--}}
-{{--@endsection--}}
-
 @extends('layout.user-layout')
-<title>E&K - Article</title>
+<title>E&K - {{$description->title}}</title>
 @section('detail')
     <div class="col-8">
-
-        //paste vô đây
+        {!! $description->Detail !!}
     </div>
 @endsection
+
+{{--@extends('layout.user-layout')--}}
+{{--<title>E&K - Article</title>--}}
+{{--@section('detail')--}}
+{{--    --}}
+{{--@endsection--}}
 
